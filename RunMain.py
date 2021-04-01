b6c957016a36e113efc81eae00e136ae98116668
jjaram117 <61059935+jjaram117@users.noreply.github.com>
2021-04-01 16:26:27 -0400

    Update RunMain.py

--- RunMain.py
+++ RunMain.py
@@ -348,7 +348,7 @@ def CalcMoveSwift(a, d): #Uses the data from GrabPts to determine what angle to 
         DesAng = 0 
         DesDist = 0
 
-        return success, DesAng, DesDist
+    return success, DesAng, DesDist
 
 
     ##----Iterate to find the first cluster where median distance surpasses DistIdeal
